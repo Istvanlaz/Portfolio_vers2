850750c83c28c6e646fad77962af84e239fcd6f0
Istvan Lazar <istvanlaz@gmail.com>
2020-02-25 16:38:40 +0100

    functionnal navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def about
+  end
+
+  def work
+  end
+
+  def art_work
+  end
 end
--- app/javascript/controllers/nav_slide_controller.js
+++ app/javascript/controllers/nav_slide_controller.js
@@ -1,16 +1,28 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.navbar-links');
+  const navMedia = document.querySelector('.social-media-links');
   const navLinks = document.querySelectorAll('.navbar-item')
+  const mediaLinks = document.querySelectorAll('.navbar-social-item')
   burger.addEventListener('click', () => {
     // Toggle Nav
     nav.classList.toggle('nav-active');
+    // Toggle Nav Social Media
+    navMedia.classList.toggle('nav-active');
     // Animate Links
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+    // Animate Social Media
+    mediaLinks.forEach((media, index) => {
+      if (media.style.animation) {
+        media.style.animation = '';
+      } else {
+        media.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
       }
     });
     // Burger Animation
